c7f94ead5b51c86e0b56b825d11b341765fefd7b
yiying2017 <417132187@qq.com>
2018-01-19 10:05:17 +0000

    static-pages

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/foo', function () {
-    return 'hello World';
-});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
