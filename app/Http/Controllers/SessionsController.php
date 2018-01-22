02f8b41da4a965cfbbe484093b92c9c465187806
2017yiying <30999849+2017yiying@users.noreply.github.com>
2018-01-22 18:56:00 +0800

    8.4.2

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,13 @@ use Auth;
 
 class SessionsController extends Controller
 {
+
+    public function __construct()
+    {
+       $this->middleware('guest', [
+           'only' => ['create']
+       ]);
+    }
     //登陆
     public function create()
     {
