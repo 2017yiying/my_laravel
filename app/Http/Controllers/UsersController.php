870415ab0ace78cfb1acbb54b3de10795f625acd
2017yiying <30999849+2017yiying@users.noreply.github.com>
2018-01-23 11:24:27 +0800

    8.4.1

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,6 +23,7 @@ class UsersController extends Controller
     //用户列表
     public function index()
     {
+        echo 'test';
         $users = User::all();
         return view('users.index', compact('users'));
     }
